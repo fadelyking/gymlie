939afbc59e141a07a78513fb4e4130a74100d890
fadelyking <fadely195@gmail.com>
2024-06-09 21:07:47 +0300

    responsiveness to sidebar

--- app/components/dashboard/SidebarItems.tsx
+++ app/components/dashboard/SidebarItems.tsx
@@ -4,53 +4,53 @@ import { FaUsers } from "react-icons/fa";
 import { BsCalendar2DateFill } from "react-icons/bs";
 import { IoMdSettings } from "react-icons/io";
 import { TbSwitch2 } from "react-icons/tb";
-export default function SidebarItems() {
-    return (
-        <div className="flex flex-col gap-5 content-between justify-between">
-            <div>
-                <ul className="flex flex-col gap-4">
-                    <li className="flex hover:bg-slate-300/75 cursor-pointer px-2.5 py-3 rounded-md transition-colors duration-75">
-                        <div className="flex gap-5 items-center flex-grow">
-                            <TbLayoutDashboardFilled size={30} />
-                            <span className=" pt-1">Dashboard</span>
-                        </div>
-                    </li>
-                    <li className="flex hover:bg-slate-300/75 cursor-pointer px-2.5 py-3 rounded-md transition-colors duration-75">
-                        <div className="flex gap-5 items-center flex-grow">
-                            <MdAnalytics size={30} />
-                            <span className=" pt-1">Analytics</span>
-                        </div>
-                    </li>
-                    <li className="flex hover:bg-slate-300/75 cursor-pointer px-2.5 py-3 rounded-md transition-colors duration-75">
-                        <div className="flex gap-5 items-center flex-grow">
-                            <FaUsers size={30} />
-                            <span className=" pt-1">Members</span>
-                        </div>
-                    </li>
-                    <li className="flex  hover:bg-slate-300/75 cursor-pointer px-2.5 py-3 rounded-md transition-colors duration-75">
-                        <div className="flex gap-5 items-center flex-grow">
-                            <BsCalendar2DateFill size={30} />
-                            <span className=" pt-1">Schedules</span>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-            <div>
-                <ul className="flex flex-col gap-4">
-                    <li className="flex hover:bg-slate-300/75 cursor-pointer px-2.5 py-3 rounded-md transition-colors duration-75'">
-                        <div className="flex gap-5 items-center flex-grow">
-                            <IoMdSettings size={30} />
-                            <span className=" pt-1">Settings</span>
-                        </div>
-                    </li>
-                    <li className="flex  hover:bg-slate-300/75 cursor-pointer px-2.5 py-3 rounded-md transition-colors duration-75">
-                        <div className="flex gap-5 items-center flex-grow">
-                            <TbSwitch2 size={30} />
-                            <span className=" pt-1">Switch Gym</span>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    );
+export default function SidebarItems({ size }: { size: number }) {
+	return (
+		<div className="flex flex-col gap-5 content-between justify-between">
+			<div>
+				<ul className="flex flex-col gap-4">
+					<li className="flex hover:bg-slate-300/75 cursor-pointer px-2.5 py-3 rounded-md transition-colors duration-75">
+						<div className="flex gap-5 items-center flex-grow">
+							<TbLayoutDashboardFilled size={size || 20} />
+							<span className=" pt-1">Dashboard</span>
+						</div>
+					</li>
+					<li className="flex hover:bg-slate-300/75 cursor-pointer px-2.5 py-3 rounded-md transition-colors duration-75">
+						<div className="flex gap-5 items-center flex-grow">
+							<MdAnalytics size={size || 20} />
+							<span className=" pt-1">Analytics</span>
+						</div>
+					</li>
+					<li className="flex hover:bg-slate-300/75 cursor-pointer px-2.5 py-3 rounded-md transition-colors duration-75">
+						<div className="flex gap-5 items-center flex-grow">
+							<FaUsers size={size || 20} />
+							<span className=" pt-1">Members</span>
+						</div>
+					</li>
+					<li className="flex  hover:bg-slate-300/75 cursor-pointer px-2.5 py-3 rounded-md transition-colors duration-75">
+						<div className="flex gap-5 items-center flex-grow">
+							<BsCalendar2DateFill size={size || 20} />
+							<span className=" pt-1">Schedules</span>
+						</div>
+					</li>
+				</ul>
+			</div>
+			<div>
+				<ul className="flex flex-col gap-4">
+					<li className="flex hover:bg-slate-300/75 cursor-pointer px-2.5 py-3 rounded-md transition-colors duration-75'">
+						<div className="flex gap-5 items-center flex-grow">
+							<IoMdSettings size={size || 20} />
+							<span className=" pt-1">Settings</span>
+						</div>
+					</li>
+					<li className="flex md:text-sm lg:text-lg hover:bg-slate-300/75 cursor-pointer px-2.5 py-3 rounded-md transition-colors duration-75">
+						<div className="flex gap-5 items-center flex-grow">
+							<TbSwitch2 size={size || 20} />
+							<span className=" pt-1">Switch Gym</span>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
 }
