4eb852847ad9ad6fec984acd92d6fe687cf6699f
fadelyking <fadely195@gmail.com>
2024-06-09 22:55:47 +0300

    get rid of unused prop

--- app/components/dashboard/SidebarItems.tsx
+++ app/components/dashboard/SidebarItems.tsx
@@ -4,32 +4,32 @@ import { FaUsers } from "react-icons/fa";
 import { BsCalendar2DateFill } from "react-icons/bs";
 import { IoMdSettings } from "react-icons/io";
 import { TbSwitch2 } from "react-icons/tb";
-export default function SidebarItems({ size }: { size: number }) {
+export default function SidebarItems() {
 	return (
 		<div className="flex flex-col gap-5 content-between justify-between">
 			<div>
 				<ul className="flex flex-col gap-4">
 					<li className="flex hover:bg-slate-300/75 cursor-pointer px-2.5 py-3 rounded-md transition-colors duration-75">
 						<div className="flex gap-5 items-center flex-grow">
-							<TbLayoutDashboardFilled size={size || 20} />
+							<TbLayoutDashboardFilled size={20} />
 							<span className=" pt-1">Dashboard</span>
 						</div>
 					</li>
 					<li className="flex hover:bg-slate-300/75 cursor-pointer px-2.5 py-3 rounded-md transition-colors duration-75">
 						<div className="flex gap-5 items-center flex-grow">
-							<MdAnalytics size={size || 20} />
+							<MdAnalytics size={20} />
 							<span className=" pt-1">Analytics</span>
 						</div>
 					</li>
 					<li className="flex hover:bg-slate-300/75 cursor-pointer px-2.5 py-3 rounded-md transition-colors duration-75">
 						<div className="flex gap-5 items-center flex-grow">
-							<FaUsers size={size || 20} />
+							<FaUsers size={20} />
 							<span className=" pt-1">Members</span>
 						</div>
 					</li>
 					<li className="flex  hover:bg-slate-300/75 cursor-pointer px-2.5 py-3 rounded-md transition-colors duration-75">
 						<div className="flex gap-5 items-center flex-grow">
-							<BsCalendar2DateFill size={size || 20} />
+							<BsCalendar2DateFill size={20} />
 							<span className=" pt-1">Schedules</span>
 						</div>
 					</li>
@@ -39,13 +39,13 @@ export default function SidebarItems({ size }: { size: number }) {
 				<ul className="flex flex-col gap-4">
 					<li className="flex hover:bg-slate-300/75 cursor-pointer px-2.5 py-3 rounded-md transition-colors duration-75'">
 						<div className="flex gap-5 items-center flex-grow">
-							<IoMdSettings size={size || 20} />
+							<IoMdSettings size={20} />
 							<span className=" pt-1">Settings</span>
 						</div>
 					</li>
 					<li className="flex md:text-sm lg:text-lg hover:bg-slate-300/75 cursor-pointer px-2.5 py-3 rounded-md transition-colors duration-75">
 						<div className="flex gap-5 items-center flex-grow">
-							<TbSwitch2 size={size || 20} />
+							<TbSwitch2 size={20} />
 							<span className=" pt-1">Switch Gym</span>
 						</div>
 					</li>
