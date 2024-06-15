7abad2cdd37675896c9e036119243b2828898350
fadelyking <fadely195@gmail.com>
2024-06-15 13:45:27 +0300

    inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className=" py-6 px-5 bg-slate-200 flex gap-5 h-auto max-w-screen-2xl mx-auto">
-				{children}
+				<main className={inter.className}>{children}</main>
 			</body>
 		</html>
 	);
