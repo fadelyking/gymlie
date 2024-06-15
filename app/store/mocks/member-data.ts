8b8aa17df585a134234a427aacfbafd44a772605
fadelyking <fadely195@gmail.com>
2024-06-15 07:51:33 +0300

    dynamic data generated with faker and gpt

--- app/store/mocks/member-data.ts
+++ app/store/mocks/member-data.ts
@@ -2,10 +2,10 @@ import { faker } from "@faker-js/faker";
 
 interface member {
 	created: string;
-	expires: Date;
+	expires: string;
 	firstName: string;
 	lastName: string;
-	phoneNumber: number;
+	phoneNumber: string;
 	birthDate: Date;
 	membership: number;
 }
@@ -35,128 +35,179 @@ interface financialData {
 const fakeMembers: Array<member> = [
 	{
 		created: faker.date.past().toString(),
-		expires: faker.date.future(),
-		firstName: faker.person.firstName(),
+		expires: faker.date.future().toString(),
+		firstName: faker.person.firstName().toString(),
 		lastName: faker.person.lastName(),
-		phoneNumber: parseInt(faker.phone.number()),
+		phoneNumber: `3${parseInt(faker.phone.number("#######"))}`,
 		birthDate: faker.date.past(),
 		membership: 1,
 	},
 	{
 		created: faker.date.past().toString(),
-		expires: faker.date.future(),
+		expires: faker.date.future().toString(),
 		firstName: faker.person.firstName(),
 		lastName: faker.person.lastName(),
-		phoneNumber: parseInt(faker.phone.number()),
+		phoneNumber: `3${parseInt(faker.phone.number("#######"))}`,
 		birthDate: faker.date.past(),
 		membership: 1,
 	},
 	{
 		created: faker.date.past().toString(),
-		expires: faker.date.future(),
+		expires: faker.date.future().toString(),
 		firstName: faker.person.firstName(),
 		lastName: faker.person.lastName(),
-		phoneNumber: parseInt(faker.phone.number()),
+		phoneNumber: `3${parseInt(faker.phone.number("#######"))}`,
 		birthDate: faker.date.past(),
-		membership: 1,
+		membership: 0,
 	},
 	{
 		created: faker.date.past().toString(),
-		expires: faker.date.future(),
+		expires: faker.date.future().toString(),
 		firstName: faker.person.firstName(),
 		lastName: faker.person.lastName(),
-		phoneNumber: parseInt(faker.phone.number()),
+		phoneNumber: `3${parseInt(faker.phone.number("#######"))}`,
 		birthDate: faker.date.past(),
 		membership: 1,
 	},
 ];
 
-const fakeMemberData: Array<newMember> = [
-	{ date: "2023-06-01", "New Members": 153 },
-	{ date: "2023-07-01", "New Members": 105 },
-	{ date: "2023-08-01", "New Members": 78 },
-	{ date: "2023-09-01", "New Members": 191 },
-	{ date: "2023-10-01", "New Members": 200 },
-	{ date: "2023-11-01", "New Members": 232 },
-	{ date: "2023-12-01", "New Members": 300 },
-];
-
-const fakeReturningMemberData: Array<returningMember> = [
-	{ date: "2023-06-01", "Returning Members": 21 },
-	{ date: "2023-07-01", "Returning Members": 43 },
-	{ date: "2023-08-01", "Returning Members": 63 },
-	{ date: "2023-09-01", "Returning Members": 20 },
-	{ date: "2023-10-01", "Returning Members": 80 },
-	{ date: "2023-11-01", "Returning Members": 10 },
-	{ date: "2023-12-01", "Returning Members": 123 },
-];
-
-const fakeSubscriptionPlanData: Array<subscriptionPlanData> = [
-	{ date: "2023-06-01", "1 Month": 31, "3 Months": 153, "6 Months": 10 },
-	{ date: "2023-07-01", "1 Month": 5, "3 Months": 21, "6 Months": 65 },
-	{ date: "2023-08-01", "1 Month": 6, "3 Months": 32, "6 Months": 21 },
-	{ date: "2023-09-01", "1 Month": 12, "3 Months": 74, "6 Months": 80 },
-	{ date: "2023-10-01", "1 Month": 45, "3 Months": 140, "6 Months": 50 },
-	{ date: "2023-11-01", "1 Month": 15, "3 Months": 55, "6 Months": 95 },
-	{ date: "2023-12-01", "1 Month": 28, "3 Months": 77, "6 Months": 40 },
-	{ date: "2024-01-01", "1 Month": 100, "3 Months": 200, "6 Months": 320 },
-	{ date: "2024-02-01", "1 Month": 80, "3 Months": 140, "6 Months": 230 },
-];
-
-const fakeMonthlySalesData: Array<financialData> = [
+const fakeMemberData = [
 	{
-		month: "January",
-		sales: 1200,
+		date: "2023-06-01",
+		"New Members": faker.number.int({ min: 50, max: 300 }),
 	},
 	{
-		month: "February",
-		sales: 1500,
+		date: "2023-07-01",
+		"New Members": faker.number.int({ min: 50, max: 300 }),
 	},
 	{
-		month: "March",
-		sales: 1800,
+		date: "2023-08-01",
+		"New Members": faker.number.int({ min: 50, max: 300 }),
 	},
 	{
-		month: "April",
-		sales: 1600,
+		date: "2023-09-01",
+		"New Members": faker.number.int({ min: 50, max: 300 }),
 	},
 	{
-		month: "May",
-		sales: 1700,
+		date: "2023-10-01",
+		"New Members": faker.number.int({ min: 50, max: 300 }),
 	},
 	{
-		month: "June",
-		sales: 1400,
+		date: "2023-11-01",
+		"New Members": faker.number.int({ min: 50, max: 300 }),
 	},
 	{
-		month: "July",
-		sales: 2000,
-	},
-	{
-		month: "August",
-		sales: 1900,
-	},
-	{
-		month: "September",
-		sales: 2100,
-	},
-	{
-		month: "October",
-		sales: 2200,
-	},
-	{
-		month: "November",
-		sales: 2300,
-	},
-	{
-		month: "December",
-		sales: 2400,
+		date: "2023-12-01",
+		"New Members": faker.number.int({ min: 50, max: 300 }),
 	},
 ];
 
+const fakeReturningMemberData = [
+	{
+		date: "2023-06-01",
+		"Returning Members": faker.number.int({ min: 10, max: 123 }),
+	},
+	{
+		date: "2023-07-01",
+		"Returning Members": faker.number.int({ min: 10, max: 123 }),
+	},
+	{
+		date: "2023-08-01",
+		"Returning Members": faker.number.int({ min: 10, max: 123 }),
+	},
+	{
+		date: "2023-09-01",
+		"Returning Members": faker.number.int({ min: 10, max: 123 }),
+	},
+	{
+		date: "2023-10-01",
+		"Returning Members": faker.number.int({ min: 10, max: 123 }),
+	},
+	{
+		date: "2023-11-01",
+		"Returning Members": faker.number.int({ min: 10, max: 123 }),
+	},
+	{
+		date: "2023-12-01",
+		"Returning Members": faker.number.int({ min: 10, max: 123 }),
+	},
+];
+
+const fakeSubscriptionPlanData = [
+	{
+		date: "2023-06-01",
+		"1 Month": faker.number.int({ min: 5, max: 100 }),
+		"3 Months": faker.number.int({ min: 21, max: 200 }),
+		"6 Months": faker.number.int({ min: 10, max: 320 }),
+	},
+	{
+		date: "2023-07-01",
+		"1 Month": faker.number.int({ min: 5, max: 100 }),
+		"3 Months": faker.number.int({ min: 21, max: 200 }),
+		"6 Months": faker.number.int({ min: 10, max: 320 }),
+	},
+	{
+		date: "2023-08-01",
+		"1 Month": faker.number.int({ min: 5, max: 100 }),
+		"3 Months": faker.number.int({ min: 21, max: 200 }),
+		"6 Months": faker.number.int({ min: 10, max: 320 }),
+	},
+	{
+		date: "2023-09-01",
+		"1 Month": faker.number.int({ min: 5, max: 100 }),
+		"3 Months": faker.number.int({ min: 21, max: 200 }),
+		"6 Months": faker.number.int({ min: 10, max: 320 }),
+	},
+	{
+		date: "2023-10-01",
+		"1 Month": faker.number.int({ min: 5, max: 100 }),
+		"3 Months": faker.number.int({ min: 21, max: 200 }),
+		"6 Months": faker.number.int({ min: 10, max: 320 }),
+	},
+	{
+		date: "2023-11-01",
+		"1 Month": faker.number.int({ min: 5, max: 100 }),
+		"3 Months": faker.number.int({ min: 21, max: 200 }),
+		"6 Months": faker.number.int({ min: 10, max: 320 }),
+	},
+	{
+		date: "2023-12-01",
+		"1 Month": faker.number.int({ min: 5, max: 100 }),
+		"3 Months": faker.number.int({ min: 21, max: 200 }),
+		"6 Months": faker.number.int({ min: 10, max: 320 }),
+	},
+	{
+		date: "2024-01-01",
+		"1 Month": faker.number.int({ min: 5, max: 100 }),
+		"3 Months": faker.number.int({ min: 21, max: 200 }),
+		"6 Months": faker.number.int({ min: 10, max: 320 }),
+	},
+	{
+		date: "2024-02-01",
+		"1 Month": faker.number.int({ min: 5, max: 100 }),
+		"3 Months": faker.number.int({ min: 21, max: 200 }),
+		"6 Months": faker.number.int({ min: 10, max: 320 }),
+	},
+];
+
+const fakeMonthlySalesData = [
+	{ month: "January", sales: faker.number.int({ min: 1200, max: 2400 }) },
+	{ month: "February", sales: faker.number.int({ min: 1200, max: 2400 }) },
+	{ month: "March", sales: faker.number.int({ min: 1200, max: 2400 }) },
+	{ month: "April", sales: faker.number.int({ min: 1200, max: 2400 }) },
+	{ month: "May", sales: faker.number.int({ min: 1200, max: 2400 }) },
+	{ month: "June", sales: faker.number.int({ min: 1200, max: 2400 }) },
+	{ month: "July", sales: faker.number.int({ min: 1200, max: 2400 }) },
+	{ month: "August", sales: faker.number.int({ min: 1200, max: 2400 }) },
+	{ month: "September", sales: faker.number.int({ min: 1200, max: 2400 }) },
+	{ month: "October", sales: faker.number.int({ min: 1200, max: 2400 }) },
+	{ month: "November", sales: faker.number.int({ min: 1200, max: 2400 }) },
+	{ month: "December", sales: faker.number.int({ min: 1200, max: 2400 }) },
+];
 export {
 	fakeMemberData,
 	fakeSubscriptionPlanData,
 	fakeReturningMemberData,
 	fakeMonthlySalesData,
+	fakeMembers,
 };
