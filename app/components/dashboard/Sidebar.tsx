49d927c91445bbc4da987b2331eca31051a4f2d3
fadelyking <fadely195@gmail.com>
2024-06-20 10:40:15 +0300

    changed title

--- app/components/dashboard/Sidebar.tsx
+++ app/components/dashboard/Sidebar.tsx
@@ -1,7 +1,7 @@
 export default function Sidebar({ children }: { children: React.ReactNode }) {
-	return (
-		<div className="hidden md:text-xs lg:flex lg:text-sm xl:text-lg rounded-2xl overflow-hidden drop-shadow-md bg-white max-w-64 h-full p-3 row-span-12 col-span-2">
-			{children}
-		</div>
-	);
+    return (
+        <div className="hidden md:text-xs lg:flex lg:text-sm xl:text-lg rounded-2xl overflow-hidden drop-shadow-md bg-white max-w-64 h-full p-3 row-span-12 col-span-2">
+            {children}
+        </div>
+    );
 }
