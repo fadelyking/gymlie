fb7617bec93a5446706bced1d4d19dd3f97dfa68
fadelyking <fadely195@gmail.com>
2024-06-23 12:39:22 +0300

    setup activity tab

---
+++ app/dashboard/activity/page.tsx
@@ -0,0 +1,11 @@
+import Sidebar from "@/app/components/dashboard/Sidebar";
+import SidebarItems from "@/app/components/dashboard/SidebarItems";
+import RootLayout from "@/app/components/dashboard/layout";
+
+export default function Page() {
+    <RootLayout>
+        <Sidebar>
+            <SidebarItems />
+        </Sidebar>
+    </RootLayout>;
+}
