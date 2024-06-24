8cb6d8b491201a15038c327acb853dd34e8fec70
fadelyking <fadely195@gmail.com>
2024-06-24 11:49:45 +0300

    add height full on small screens

--- app/components/dashboard/Sidebar.tsx
+++ app/components/dashboard/Sidebar.tsx
@@ -1,6 +1,6 @@
 export default function Sidebar({ children }: { children: React.ReactNode }) {
     return (
-        <div className="hidden xl:flex xl:text-md 2xl:text-lg rounded-2xl overflow-hidden drop-shadow-md bg-white max-w-64 h-full p-3 row-span-12 col-span-2">
+        <div className="hidden xl:flex xl:text-md 2xl:text-lg rounded-2xl overflow-hidden grow drop-shadow-md bg-white max-w-64 h-full p-3 row-span-12 col-span-2">
             {children}
         </div>
     );
