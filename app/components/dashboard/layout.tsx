12079bba4907f6d8f02381159f06c1f75c28437a
fadelyking <fadely195@gmail.com>
2024-06-25 10:57:23 +0300

    sidebar added to layout

--- app/components/dashboard/layout.tsx
+++ app/components/dashboard/layout.tsx
@@ -1,10 +1,16 @@
+import Sidebar from "../shared/Sidebar";
+import SidebarItems from "../shared/SidebarItems";
+
 export default function RootLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
     return (
-        <div className="flex flex-col gap-5 mx-auto sm:grid sm:grid-cols-12 sm:h-full sm:grid-rows-16 sm:gap-5 max-w-80 sm:max-w-screen-2xl xl:grid-rows-13">
+        <div className="flex flex-col justify-center p-6 gap-5 max-w-80 mx-auto sm:grid sm:grid-cols-12 sm:min-h-full sm:grid-rows-16 sm:max-w-screen-2xl xl:grid-rows-13">
+            <Sidebar>
+                <SidebarItems />
+            </Sidebar>
             {children}
         </div>
     );
