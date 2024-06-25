886e3f03dff138dcc076537358afcc52e20dc22b
fadelyking <fadely195@gmail.com>
2024-06-25 10:39:44 +0300

    include sidebar in layout rather than separate pages

--- app/components/dashboard/Sidebar.tsx
+++
@@ -1,7 +0,0 @@
-export default function Sidebar({ children }: { children: React.ReactNode }) {
-    return (
-        <div className="hidden xl:flex xl:text-md 2xl:text-lg rounded-2xl overflow-hidden grow drop-shadow-md bg-white max-w-64 h-full p-3 row-span-12 col-span-2">
-            {children}
-        </div>
-    );
-}
